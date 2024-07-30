65b20da0425dc2490537360747e122a00e4864dd
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-07-30 17:48:27 -0500

    Optimized emoji string

--- src/commands/abilities.js
+++ src/commands/abilities.js
@@ -2,12 +2,14 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { abilities } = require('../abilities');
 const embedColors = require("../embedColors.js")
 
+let emojiString = undefined
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('abilities')
     .setDescription('Displays every ability in Bopl Battle!'),
   async execute(interaction) {
-    var emojiString = abilities.map((emoji) => `${interaction.client.emojis.cache.get(emoji)} `).join('');
+    if(emojiString==undefined)emojiString = abilities.map((emoji) => `${interaction.client.emojis.cache.get(emoji)} `).join('');
+
     await interaction.reply({
       embeds: [
         new EmbedBuilder()
